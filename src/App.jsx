bcdab65f2dd021baa27ed773f6afb257f3d21cd2
atran-ts <atran@twinstarcu.com>
2020-10-15 16:13:37 -0700

    getLocation and api call for openweather working

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  apikey = 'e3d1ce1163b9ce15e2c2477431d0e3e2';
   constructor(props) {
     super(props);
     this.state = {
     };
   }
+
+  getByCoords(data) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + data.latitude + "&lon=" + data.longitude + "&appid=" + this.apikey)
+      .then((res) => res.json())
+      .then((result => {
+        console.log(result);
+      }))
+  }
+
+  getlocation() {
+    let promise = new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        resolve(position);
+      });
+    }).then(data => {
+      this.getByCoords(data.coords);
+    })
+  }
+
   componentDidMount() {
     if ("geolocation" in navigator) {
       console.log("Available");
     } else {
       console.log("Not Available");
     }
-
-    navigator.geolocation.getCurrentPosition(function (position) {
-      console.log(position)
-    });
+    this.getlocation();
   }
   render() {
     return (
